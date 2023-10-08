22a3e7041204acf8261552f07ab21a46beba1cd8
uspapon <papon.fsp@gmail.com>
2023-10-08 23:14:56 +0600

    condetional rendering for loging has been done

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -5,11 +5,13 @@ import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home";
 import Login from "../Pages/Login/Login";
 import Register from "../Pages/Register/Register";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
 
 
 export const router = createBrowserRouter([
   {
     path: "/",
+    error: <ErrorPage></ErrorPage>,
     element: <Main></Main>,
     children: [
       {
