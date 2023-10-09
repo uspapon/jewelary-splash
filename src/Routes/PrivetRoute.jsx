4dcc0a25352861ba128ff0d10a2c22e91758b609
uspapon <papon.fsp@gmail.com>
2023-10-09 12:31:25 +0600

    implementing private route

---
+++ src/Routes/PrivetRoute.jsx
@@ -0,0 +1,14 @@
+import { useContext } from "react";
+import { AuthContext } from "../providers/AuthProviders";
+import { Navigate } from "react-router-dom";
+
+const PrivetRoute = ({ children }) => {
+    const { user } = useContext(AuthContext);
+
+    if(user) {
+        return children; 
+    }
+    return <Navigate to='/login'></Navigate>
+};
+
+export default PrivetRoute;
