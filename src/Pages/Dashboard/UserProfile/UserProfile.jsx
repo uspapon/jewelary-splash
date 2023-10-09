ea13ddc9364a6d8fb7e8ea19241493a588b68f8e
uspapon <papon.fsp@gmail.com>
2023-10-09 13:58:54 +0600

    implementing dashboard route

---
+++ src/Pages/Dashboard/UserProfile/UserProfile.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserProfile = () => {
+    return (
+        <div className='p-10'>
+            <h2 className="text-2xl">Profile</h2>
+            <p>Welcome! User</p>
+        </div>
+    );
+};
+
+export default UserProfile;
