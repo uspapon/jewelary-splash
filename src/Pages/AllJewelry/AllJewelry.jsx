4765a8192e30d1e92b364cfe8cd82e6fffa88373
uspapon <papon.fsp@gmail.com>
2023-10-09 18:53:19 +0600

    showing jewelry data from mongodb to the All jewery page has been done

--- src/Pages/AllJewelry/AllJewelry.jsx
+++ src/Pages/AllJewelry/AllJewelry.jsx
@@ -1,17 +1,26 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Jewelry from '../Jewelry/Jewelry';
 
 const AllJewelry = () => {
+    const [jewelry, setJewlry] = useState([]);
 
-    useEffect(() => {
-        
+    useEffect( () => {
+        fetch('http://localhost:5000/alljewelry')
+        .then(res => res.json())
+        .then(data => setJewlry(data))
+
     },[])
 
     return (
         <div className='p-10'>
             
             <div className='grid md:grid-cols-3 gap-5'>
-                <Jewelry></Jewelry>
+                {
+                    jewelry.map(item => <Jewelry
+                                key={item._id}
+                                item={item}
+                    ></Jewelry>)
+                }
 
             </div>
 
--- src/Pages/Jewelry/Jewelry.jsx
+++ src/Pages/Jewelry/Jewelry.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 
-const Jewelry = () => {
+const Jewelry = ({item}) => {
+    const { _id, jewelryName, jewelryImg, jewelryPrice } = item;
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
-            <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+            <figure><img className='h-[250px]' src={jewelryImg} alt="Shoes" /></figure>
             <div className="card-body">
                 <h2 className="card-title">
-                    Shoes!
+                    {jewelryName}
                     <div className="badge badge-secondary">NEW</div>
                 </h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <p>${jewelryPrice}</p>
                 <div className="card-actions justify-end">
                     <div className="badge badge-outline">Fashion</div>
                     <div className="badge badge-outline">Products</div>
